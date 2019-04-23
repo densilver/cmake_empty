01da9d7ba63d1c0fad20b0df4e254ac8699a21fd
Denis Serebro <densilver3000@gmail.com>
2019-04-24 00:35:47 +0300

    initial vscode

--- build.sh
+++ build.sh
@@ -5,22 +5,23 @@ CMAKE_BIN=cmake
 BUILD_TYPE=Debug
 
 PROJECT_DIR=`pwd`
-BUILD_DIR=build/${BUILD_TYPE}
+BUILD_DIR=.build/${BUILD_TYPE}
+BIN_DIR=${BUILD_DIR}/bin
 
-if [[ "xrebuild" == "x$1" ]]; then
+if [[ "xrebuild" == "x${1}" ]]; then
 	rm -rf ${BUILD_DIR}
 fi
-mkdir -p ${BUILD_DIR}
+${CMAKE_BIN} -E make_directory ${BUILD_DIR}
+
 cd ${BUILD_DIR}
-
 ${CMAKE_BIN} -DCMAKE_BUILD_TYPE=${BUILD_TYPE} ${PROJECT_DIR}
-${CMAKE_BIN} --build . -- -j4
-
-mkdir -p bin
-cp -v src/app/app bin
-
 cd ${PROJECT_DIR}
+
+${CMAKE_BIN} --build ${BUILD_DIR} -- -j 8
+
+${CMAKE_BIN} -E make_directory ${BIN_DIR}
+${CMAKE_BIN} -E copy ${BUILD_DIR}/src/app/app ${BIN_DIR}
 
 echo
 echo run:
-echo ./${BUILD_DIR}/bin/app
+echo ./${BIN_DIR}/app
