152c04e4141320b57b40132f0c1d05f9cbf52912
Denis Serebro <densilver3000@gmail.com>
2019-11-28 23:07:23 +0300

    refs

--- src/app/src/main.cpp
+++ src/app/src/main.cpp
@@ -1,18 +1,82 @@
-#include <cstdint>
-#include <limits>
+#include <stdio.h>
 
-#include <iostream>
+struct Node;
+struct Node {
+	int val;
+	Node* next;
+};
 
-static void test() {
-	constexpr unsigned int psz = sizeof(void *);
-	constexpr auto n = std::numeric_limits<std::uint64_t>::max() / (1ull << (psz == 4 ? 32 : 3));
+struct List {
+public:
+	Node* new_node(int val) {
+		Node* p = new Node;
+		p->val = val;
+		p->next = nullptr;
+		return p;
+	}
+	
+	Node* add_front(Node* p) {
+		p->next = head;
+		head = p;
+		return p;
+	}
 
-	char* p = new(std::nothrow) char[n];
-	std::cout << "psz: " << psz << "\n";
-	std::cout << "n: " << n << "\n";
-	std::cout << "p: " << static_cast<void *>(p) << "\n";
-}
+	Node* add_end(Node* newp) {
+		if (!head) {
+			head = newp;
+			return head;
+		}
+		Node* p = head;
+		while (p->next) {
+			p = p->next;
+		}
+		p->next = newp;
+		return head;
+	}
+
+	Node* lookup(int val) {
+		for (Node* p = head; p; p = p->next) {
+			if (p->val == val) {
+				return p;
+			}
+		}
+		return nullptr;
+	}
+
+	Node* del_node(int val) {
+		Node* prev = nullptr;
+		for (Node* p = head; p; p = p->next) {
+			if (p->val == val) {
+				if (prev == nullptr) {
+					head = p->next;
+				} else {
+					prev->next = p->next;
+				}
+				delete p;
+				return head;
+			}
+			prev = p;
+		}
+		return head;
+	}
+	
+	~List() {
+		for (Node* next; head; head = next) {
+			next = head->next;
+			delete head;
+		}
+	}
+
+private:
+	Node* head = nullptr;
+};
 
 int main() {
-	test();
+	List nodes;
+
+	nodes.add_front(nodes.new_node(0));
+	nodes.add_front(nodes.new_node(1));
+	nodes.add_front(nodes.new_node(2));
+
+	nodes.del_node(1);
 }
